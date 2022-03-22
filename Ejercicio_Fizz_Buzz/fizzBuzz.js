ef18c128620b86c34e40e2b609bc329b1d4a1d76
LucianoAimaretti <lucianoaimaretti@gmail.com>
2022-03-22 14:01:16 -0300

    Ejercicio practico

---
+++ Ejercicio_Fizz_Buzz/fizzBuzz.js
@@ -0,0 +1,18 @@
+function includeNumber(number, compareNumber) {
+    let str = number.toString();
+    return str.includes(compareNumber);
+}
+
+function isDivisible(list) {
+    let stringList = [];
+    for (let i = 0; i < list.length; i++) {
+        list[i] % 3 == 0 && list[i] % 5 == 0
+            ? stringList.push("FizzBuzz")
+            : list[i] % 3 == 0 || includeNumber(list[i], 3)
+            ? stringList.push("Fizz")
+            : list[i] % 5 == 0 || includeNumber(list[i], 5)
+            ? stringList.push("Buzz")
+            : stringList.push("");
+    }
+    return stringList;
+}
