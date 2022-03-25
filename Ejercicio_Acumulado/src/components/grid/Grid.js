8e36c6ffc6ad3ac7e74bc3c011c30e9b9189c17c
LucianoAimaretti <lucianoaimaretti@gmail.com>
2022-03-25 14:29:30 -0300

    Update Grid.js

--- Ejercicio_Acumulado/src/components/grid/Grid.js
+++ Ejercicio_Acumulado/src/components/grid/Grid.js
@@ -1,13 +1,33 @@
 import "./grid.scss";
-import Card from "../article/card";
+import Card from "../article/Card";
+import React from "react";
 
-export default function Grid(data) {
+export default function Grid({ data }) {
     return (
         <section className="grid">
-            <h1 className="grid__title">Aumulado Grilla</h1>
+            <div className="grid__containaer__title">
+                <h1 className="grid__title">Aumulado Grilla</h1>
+            </div>
             {/* Agregar tags */}
-            {/* Cards */}
-            {/* <Card image={data.promo_items.url} title={data.headline.basic} subtitle={data.promo_items.subtitle} date={data.display_date} /> */}
+            {/* Cards Section */}
+            <section className="grid__card">
+                {data.map((article, index) => {
+                    if (article.subtype === "7") {
+                        return (
+                            <Card
+                                key={index}
+                                image={article.promo_items.basic.url}
+                                title={article.headlines.basic}
+                                subtitle={article.promo_items.basic.subtitle}
+                                date={article.display_date}
+                            />
+                        );
+                    }
+                    {
+                        /*  */
+                    }
+                })}
+            </section>
         </section>
     );
 }
