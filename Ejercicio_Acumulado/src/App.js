20eb4df3f4c82d3f59728afd7ae0265a5bd0630c
LucianoAimaretti <lucianoaimaretti@gmail.com>
2022-03-28 02:33:11 -0300

    Update App

--- Ejercicio_Acumulado/src/App.js
+++ Ejercicio_Acumulado/src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./sass/app.scss";
 
 import { Navbar, Grid, Footer } from "./components/index";
 
-function App(data) {
+function App() {
+    const url = "https://api-test-ln.herokuapp.com/articles";
+    const [data, setData] = useState();
+
+    const fetchApi = async () => {
+        const response = await fetch(url);
+
+        const responseJSON = await response.json();
+        setData(responseJSON);
+    };
+
+    useEffect(() => fetchApi(), []);
+
     return (
-        <main className="container">
+        <main>
             {/* Navbar Section */}
             <Navbar />
-
+            {}
             {/* Grid Section */}
-            <Grid data={data} />
+            <Grid data={data.articles} />
 
             {/* Footer Section */}
             <Footer />
